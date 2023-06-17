5db99e99adf941c0375f1e6623e93dae7bb9852c
danieelst <danielstribrand98@gmail.com>
2023-06-17 21:37:26 +0200

    Adding literals

--- scripts/test.py
+++ scripts/test.py
@@ -2,6 +2,8 @@ from tests.translation import (test_all_code_points,
                                test_gen_natural_to_natural,
                                test_natural_to_octalang)
 
+from literals import LITERALS
+
 def test(res: bool):
   if res:
     print('Passed!')
@@ -14,7 +16,5 @@ if __name__=='__main__':
   print('-- Testing translation of generated strings --')
   test(test_gen_natural_to_natural(20))
   print('-- Testing translation of strings --')
-  test(test_natural_to_octalang('Hello World',
-    '(ọọɵ)(ọǫ̂ộ)(ọộǫ̂)(ọộǫ̂)(ọộø̂)/(ọoø̂)(ọộø̂)(ọɵ̂o)(ọộǫ̂)(ọǫ̂ǫ̂)'))
-  test(test_natural_to_octalang('A constructed language',
-    '(ọɵọ)/(ọǫ̂ô)(ọộø̂)(ọộɵ̂)(ọɵ̂ô)(ọɵ̂ǫ̂)(ọɵ̂o)(ọɵ̂ộ)(ọǫ̂ô)(ọɵ̂ǫ̂)(ọǫ̂ộ)(ọǫ̂ǫ̂)/(ọộǫ̂)(ọǫ̂ọ)(ọộɵ̂)(ọǫ̂ø̂)(ọɵ̂ộ)(ọǫ̂ọ)(ọǫ̂ø̂)(ọǫ̂ộ)'))
+  for nat,octl in LITERALS.items():
+    test(test_natural_to_octalang(nat,octl))
